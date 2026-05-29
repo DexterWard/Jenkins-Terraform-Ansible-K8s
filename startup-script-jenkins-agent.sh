4d4e5e158e60ca6b382cdf71f112ebe608816b3f
DexterWard <benon.f@gmail.com>
2026-05-29 13:16:19 +0200

    Added startup script for the Jenkins agent

---
+++ startup-script-jenkins-agent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Install Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+apt update
+apt install -y terraform
+
+#Install Python
+apt install -y python3 pipx 
+
+#Install Ansible
+pipx install --include-deps ansible
+su -u ubuntu -c "pipx ensurepath"
+
+
+
