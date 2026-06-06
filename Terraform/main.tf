6e0d645f86288f3f26d49e49a782b7c870b3d7d8
DexterWard <benon.f@gmail.com>
2026-06-06 16:59:10 +0200

    Added provisioners

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,9 +20,34 @@ locals {
 resource "aws_instance" "kubeadm" {
     ami = var.ami
     instance_type = var.instance_type
+    subnet_id = "subnet-06c46458612776034"
+    private_ip = "172.31.45.24[count.index]"
     count = 2
 
     tags = {
       Name = "kubeadm-${local.ec2-name[count.index]}"
     }
+
+    connection {
+    type = "ssh"
+    user = "ubuntu"
+    host = self.public_ip
+    private_key = file("../Jenkins-Terraform-Ansible-K8s.pem")
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "sudo hostnamectl set-hostname jenkins-agent",
+        "sudo timedatectl set-timezone Europe/Amsterdam",
+        "sudo apt update",
+        "sudo apt install -y python3 pipx",
+        "pipx install --include-deps ansible",
+        "pipx ensurepath",
+     ]
+  }
+
+  provisioner "file" {
+    source      = "Ansible/hosts.ini"
+    destination = "/home/ubuntu/hosts.ini"
+  }
 }
