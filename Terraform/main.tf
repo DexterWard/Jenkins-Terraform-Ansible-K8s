8a02e3760bdbd7dc99a2e0142d2eb0c7f2f90f60
DexterWard <benon.f@gmail.com>
2026-06-17 17:48:21 +0200

    Added egress rule so the kubeadm machines can install software

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -41,6 +41,15 @@ resource "aws_vpc_security_group_ingress_rule" "ansible_ping" {
   to_port = -1
 }
 
+resource "aws_vpc_security_group_egress_rule" "egress" {
+    security_group_id = aws_security_group.k8s.id
+    from_port   = 0
+    to_port     = 0
+    ip_protocol    = "-1"
+    cidr_ipv4 = "0.0.0.0/0"
+}
+
+
 locals {
   ec2-name =  ["master","node"]
   last-digit = ["1","2"]
