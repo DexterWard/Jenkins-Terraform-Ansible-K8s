13d7f0982102d5e88d54601ebe39d2261469c188
DexterWard <benon.f@gmail.com>
2026-06-22 12:49:11 +0200

    Updated IAM settings

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -63,21 +63,34 @@ resource "aws_vpc_security_group_egress_rule" "egress" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
-resource "aws_iam_policy" "ALB_policy" {
-  name        = "alb_policy"
-  path        = "/"
-  description = "ALB policy"
+resource "aws_iam_policy" "alb_policy" {
+  name   = "alb-policy"
   policy = file("${path.module}/../AWS/aws-load-balancer-controller-policy.json")
 }
 
 resource "aws_iam_role" "kubeadm_role" {
-  name               = "kubeadm-role"
-  assume_role_policy = aws_iam_policy.ALB_policy.id
+  name = "kubeadm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
+resource "aws_iam_role_policy_attachment" "alb_attach" {
   role       = aws_iam_role.kubeadm_role.name
-  policy_arn = aws_iam_policy.ALB_policy.arn
+  policy_arn = aws_iam_policy.alb_policy.arn
+}
+
+resource "aws_iam_instance_profile" "kubeadm_profile" {
+  name = "kubeadm-profile"
+  role = aws_iam_role.kubeadm_role.name
 }
 
 locals {
