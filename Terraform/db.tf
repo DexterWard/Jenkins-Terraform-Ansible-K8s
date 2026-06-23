ac0cd44cb54e184898f5b72f68abe3c6201e4255
DexterWard <benon.f@gmail.com>
2026-06-23 11:41:44 +0200

    Updated db identifier

--- Terraform/db.tf
+++ Terraform/db.tf
@@ -1,12 +1,13 @@
 resource "aws_db_instance" "db1" {
-  db_name             = "k8s"
-  instance_class      = "db.t4g.micro"
-  allocated_storage   = "20"
-  engine              = "postgres"
-  username            = "postgres"
-  skip_final_snapshot = true
-  publicly_accessible = true
-  password            = var.db_password
+  db_name                = "k8s"
+  identifier             = "k8s"
+  instance_class         = "db.t4g.micro"
+  allocated_storage      = "20"
+  engine                 = "postgres"
+  username               = "postgres"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  password               = var.db_password
   vpc_security_group_ids = [aws_security_group.db.id]
-  depends_on = [aws_security_group.db]
+  depends_on             = [aws_security_group.db]
 }
