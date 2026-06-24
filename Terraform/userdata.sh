ef29cf520c0d1dc2f8c8056628e18a3cc0a87938
DexterWard <benon.f@gmail.com>
2026-06-24 13:25:10 +0200

    Created ecr-secret and service account to download the image from ECR

--- Terraform/userdata.sh
+++ Terraform/userdata.sh
@@ -78,7 +78,3 @@ apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-
-#Install ECR credential helper
-apt update
-apt install -y amazon-ecr-credential-helper
