fb5c17e2600daa8b19fc4d0e15469e922d4a8cb6
DexterWard <benon.f@gmail.com>
2026-06-25 21:48:41 +0200

    Updated Terraform format

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "kubeadm" {
   private_ip             = "172.31.1.${local.last-digit[count.index]}"
   key_name               = "Jenkins-Terraform-Ansible-K8s"
   iam_instance_profile   = aws_iam_instance_profile.kubeadm_profile.name
-  source_dest_check = false
+  source_dest_check      = false
 
   root_block_device {
     volume_size = 20
