697ee83a3c7e13672132ced9434b5edb8d3b452a
DexterWard <benon.f@gmail.com>
2026-06-29 14:17:37 +0200

    Fixed db subnets

--- Terraform/subnets.tf
+++ Terraform/subnets.tf
@@ -38,13 +38,13 @@ resource "aws_subnet" "public_b" {
 resource "aws_subnet" "db_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "172.31.10.0/24"
-  availability_zone = "eu-central-1b"
+  availability_zone = "eu-central-1a"
 }
 
 resource "aws_subnet" "db_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "172.31.11.0/24"
-  availability_zone = "eu-central-1c"
+  availability_zone = "eu-central-1b"
 }
 
 resource "aws_db_subnet_group" "db" {
